050a72cbbcb858524834ca4393d7918a74fef17c
Raheel Ahmad <ahmadraheel@gmail.com>
2018-02-04 16:34:03 -0800

    Use semantic-ui-react via npm

--- src/myapp/core.cljs
+++ src/myapp/core.cljs
@@ -1,9 +1,24 @@
 (ns myapp.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            ["semantic-ui-react" :refer (Button Image Container Menu MenuItem)]
+            ))
 
 (defn root []
   [:div
-   [:h1 "Hello there :)"]])
+   [:> Menu {:fixed "top"}
+    [:> Container {}
+     [:> MenuItem {:as "a" :header true}
+      [:> Image {:size "mini" :src "https://react.semantic-ui.com/logo.png"
+                 :style {:margin-right "1.5em"}}]
+      "Project Namespace"
+      ]
+     [:> MenuItem {:as "a"}
+      "Home"
+      ]
+     ]]
+   [:h1 "Hello there :)"]
+   [:> Button {} "Click me"]
+   ])
 
 (defn mount-root []
   (js/console.log "Mounting from mount-root")
