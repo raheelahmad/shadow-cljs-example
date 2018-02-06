ae8b416b40a79a076af3468d6c86ee1097a27c70
Raheel Ahmad <ahmadraheel@gmail.com>
2018-02-05 19:12:06 -0800

    Use Transition

--- src/myapp/core.cljs
+++ src/myapp/core.cljs
@@ -1,31 +1,48 @@
 (ns myapp.core
   (:require [reagent.core :as r]
-            ["semantic-ui-react" :refer (Button Image Container Menu MenuItem)]
+            [semantic-ui-react :refer (Button Image Container Menu MenuItem Transition)]
             ))
+
+(def anim-duration 300)
+
+(def showing (r/atom false))
+
+(defn fade-comp [in]
+  [:> Transition {:visible in
+                  :animation "slide up" :duration "500"
+                     }
+   [:div "I am toggled via a "
+    [:code "Transition"]
+    [:span " from "]
+    [:a {:href "https://react.semantic-ui.com"}
+     "Semantic UI React"]]])
 
 (defn root []
   [:div
-   [:> Menu {:fixed "top"}
+   [:> Menu
     [:> Container {}
      [:> MenuItem {:as "a" :header true}
       [:> Image {:size "mini" :src "https://react.semantic-ui.com/logo.png"
                  :style {:margin-right "1.5em"}}]
-      "Project Namespace"
+      "Project Name"
       ]
      [:> MenuItem {:as "a"}
-      "Home"
-      ]
-     ]]
-   [:h1 "Hello there :)"]
-   [:> Button {} "Click me"]
-   ])
+      "Home"]]]
+   [:h1 "An experiment with shadow-cljs"]
+   [:div "Click on the button below to: "]
+   [:> Button {:on-click (fn []
+                           (reset! showing (not @showing)))}
+    (if @showing "Hide" "Show")]
+   [fade-comp @showing]
+   [:ul
+    [:li "some"]
+    [:li "more"]
+    [:li "content"]]])
 
 (defn mount-root []
-  (js/console.log "Mounting from mount-root")
   (r/render [root]
             (.getElementById js/document "app")))
 
 (defn init []
-  (js/console.log "Inited from myapp.core/init")
   (mount-root))
 
